f6d8108a061d2f9002a2d6ba1de9db9d2a3a2807
CarlosEmds <carloseduardomarinho21@gmail.com>
2024-06-05 15:48:57 -0300

    Concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,14 +3,25 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo { get; set; }
+        protected string Imei { get; set; }
+        protected int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+           
+        public Smartphone(string numero, string modelo, string imei,int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
+        
+
+        
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
@@ -19,6 +30,11 @@ namespace DesafioPOO.Models
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+        }
+
+        public void InformacaoDoAparelho()
+        {
+            Console.WriteLine($"{Modelo}, {Imei} e {Memoria}");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero:"123456", modelo: "Modelo2", imei:"11111111", memoria:64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+nokia.InformacaoDoAparelho();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone");
+Smartphone iphone = new Iphone(numero: "321", modelo: "Modelo1", imei: "222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+iphone.InformacaoDoAparelho();
